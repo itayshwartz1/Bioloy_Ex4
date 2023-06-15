afe3875551a27d4473ca3cadbd34f794bd033274
Itay <itay2007@gmail.com>
2023-06-15 18:36:07 +0300

    a

--- perceptron.py
+++ perceptron.py
@@ -1,23 +1,27 @@
 import numpy as np
 import random
 
-
-
 TEST_SIZE = 0.2
 POPULATION_SIZE = 100
-MAX_GEN = 200
-ELITISM = 0.1
+MAX_GEN = 500
+ELITISM = 0.2
 TOURNAMENT_SIZE = 10
-MUTATE_RATE = 0.5
+MUTATE_RATE = 0.3
+
+global TRAIN_SIZE
+TRAIN_SIZE = 0
+
 def get_strings():
     inputs = []
     labels = []
+    global TRAIN_SIZE
 
     with open("nn0.txt") as file:
         for line in file:
             if line.strip() != "":
                 string, label = line.strip().split()
-                inputs.append(np.array([list(letter) for letter in string], dtype=int).reshape(1, -1))
+                input_with_bias = np.concatenate(([1], np.array([list(letter) for letter in string], dtype=int).flatten()))
+                inputs.append(input_with_bias)
                 labels.append(int(label))
 
     indices = np.arange(len(inputs))
@@ -25,11 +29,15 @@ def get_strings():
     shuffled_inputs = np.take(inputs, indices, axis=0)
     shuffled_labels = np.take(labels, indices, axis=0)
 
-    split_point = int(0.8 * len(inputs))  # 80% for training, 20% for testing
+    TRAIN_SIZE = int(0.8 * len(inputs))  # 80% for training, 20% for testing
 
     # Split the shuffled data into training and testing sets
-    input_train, input_test = shuffled_inputs[:split_point], shuffled_inputs[split_point:]
-    labels_train, labels_test = shuffled_labels[:split_point], shuffled_labels[split_point:]
+    input_train, input_test = shuffled_inputs[:TRAIN_SIZE], shuffled_inputs[TRAIN_SIZE:]
+    labels_train, labels_test = shuffled_labels[:TRAIN_SIZE], shuffled_labels[TRAIN_SIZE:]
+
+    # Update labels to 1 and -1
+    labels_train = np.array(labels_train) * 2 - 1
+    labels_test = np.array(labels_test) * 2 - 1
 
     return input_train, labels_train, input_test, labels_test
 
@@ -37,10 +45,7 @@ def get_strings():
 def init_population():
     population = []
     for i in range(POPULATION_SIZE):
-
-        population.append([np.random.uniform(-1, 1, size=(16, 64)),
-                           np.random.uniform(-1, 1, size=(64, 32)),
-                           np.random.uniform(-1, 1, size=(32, 1))])
+        population.append(np.random.uniform(-1, 1, size=(17, 1)))
     return population
 
 
@@ -48,29 +53,31 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
-def calculate_fitnesses(population, input, labels):
+def calculate_fitnesses(population, X, Y):
     fitness_scores = []
-    labels = np.squeeze(labels)
-    for W in population:
-        tmp = input
-        for layer in W:
-            z = np.dot(tmp, layer)
-            a = sigmoid(z)
-            tmp = a
+    for w in population:
+        hits = 0
+        for i, x in enumerate(X):
+            pred = np.dot(x, w)[0]
+            y = Y[i]
+            if pred * y > 0:
+                hits += 1
 
-        tmp = np.squeeze(tmp)
-        fitness_scores.append(np.dot(tmp - labels, tmp - labels))
+        fitness_scores.append(hits / TRAIN_SIZE)
 
     return fitness_scores
 
 
+
 def get_parents(population_with_fitnesses):
     tournament = random.sample(population_with_fitnesses, TOURNAMENT_SIZE)
-    parent1 = min(tournament, key=lambda x: x[1])
-    tournament.remove(parent1)
-    parent2 = min(tournament, key=lambda x: x[1])
+    sorted_tournament = sorted(tournament, key=lambda x: x[1], reverse=True)[::-1]
+    parent1 = sorted_tournament[0][0]
+    parent2 = sorted_tournament[1][0]
 
-    return parent1[0], parent2[0]
+    return parent1, parent2
+
+
 
 
 def unflatten(child1_genes):
@@ -88,26 +95,25 @@ def unflatten(child1_genes):
     return [genes1, genes2, genes3]
 
 def crossover(parent1, parent2):
-    genes1 = np.concatenate([a.flatten() for a in parent1])
-    genes2 = np.concatenate([a.flatten() for a in parent2])
+    genes1 = parent1
+    genes2 = parent2
     split = random.randint(0, len(genes1) - 1)
-    child1_genes = np.asarray(genes1[:split].tolist() + genes2[split:].tolist())
-    child2_genes = np.asarray(genes2[:split].tolist() + genes1[split:].tolist())
-    child1 = unflatten(child1_genes)
-    child2 = unflatten(child2_genes)
+    child1_genes = np.concatenate((genes1[:split], genes2[split:]))
+    child2_genes = np.concatenate((genes2[:split], genes1[split:]))
+    child1 = child1_genes.reshape((17, 1))
+    child2 = child2_genes.reshape((17, 1))
 
     return child1, child2
 
 
+
 def mutate(new_population):
     for index, child in enumerate(new_population):
-        if random.uniform(0, 1) < MUTATE_RATE:
-            for _ in range(50):
-                m = random.randint(0, len(child) - 1)
-                i = random.randint(0, len(child[m]) - 1)
-                j = random.randint(0, len(child[m][0]) - 1)
-                child[m][i][j] = random.uniform(-1, 1)
-            new_population[index] = child
+        for _ in range(4):
+            if random.uniform(0, 1) < MUTATE_RATE:
+                i = random.randint(0, child.size - 1)
+                child[i] = random.uniform(-1, 1)
+                new_population[index] = child
 
     return new_population
 
@@ -118,16 +124,20 @@ def genetic_algorithm():
 
     for gen in range(MAX_GEN):
         fitness_scores = calculate_fitnesses(population, input_train, labels_train)
-        sorted_indices = np.argsort(fitness_scores)
-        sorted_population = np.take(population, sorted_indices, axis=0)
+
+        sorted_indices = np.argsort(fitness_scores)[::-1]
+        tmp = np.take(population, sorted_indices, axis=0)
+        sorted_population = [np.array(l) for l in tmp]
         sorted_fitness = np.take(fitness_scores, sorted_indices, axis=0)
 
-        if sorted_fitness[0] == 0:
+        best = sorted_fitness[0]
+        if best == 1:
             print("you win!!")
             break
 
-        new_population = sorted_population[:round(ELITISM * POPULATION_SIZE)].tolist()
-        print(min(sorted_fitness))
+        new_population = sorted_population[:round(ELITISM * POPULATION_SIZE)]
+        print(str(gen) + "," + str(best))
+
         while len(new_population) < POPULATION_SIZE:
             parent1, parent2 = get_parents(list(zip(sorted_population, sorted_fitness)))
             child1, child2 = crossover(parent1, parent2)
